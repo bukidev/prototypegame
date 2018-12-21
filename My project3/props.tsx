bea259ed7de1375c46218540dad98e925b0b0ce9
Bukhari29 <dilstudio29@gmail.com>
2018-12-22 00:11:01 +0700

    initail commit

---
+++ My project3/props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="props" tilewidth="16" tileheight="16" tilecount="544" columns="34">
+ <image source="assets/environment/layers/props.png" width="544" height="256"/>
+</tileset>
